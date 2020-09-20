c648887d1d4331b2b3b02e107df6a0091c18f8d0
Jim Robinson-Bohnslav <jbohnslav@gmail.com>
2020-09-20 13:34:09 -0400

    added initial unit tests and notebook

---
+++ tests/setup_testing_directory.py
@@ -0,0 +1,47 @@
+import argparse
+import os
+from typing import Union
+import warnings
+
+def get_testing_directory() -> str:
+    directory_file = 'testing_directory.txt'
+    if os.path.isfile(directory_file):
+        with open(directory_file, 'r') as f:
+            testing_directory = f.read()
+            return testing_directory
+    else:
+        raise ValueError('please run setup_data.py before attempting to run unit tests')
+
+def setup_testing_directory(datadir: Union[str, os.PathLike], overwrite: bool = False) -> str:
+    testing_path_file = 'testing_directory.txt'
+
+    should_setup = True
+    if os.path.isfile(testing_path_file):
+        with open(testing_path_file, 'r') as f:
+            testing_directory = f.read()
+            if not os.path.isfile(testing_directory):
+                raise ValueError('saved testing directory {} does not exist, re-run ')
+                warnings.warn('Saved testing directory {} does not exist, downloading Thumos14...'.format(
+                    testing_directory
+                ))
+            else:
+                should_setup = False
+    if not should_setup:
+        return testing_directory
+
+    testing_directory = datadir
+    assert os.path.isdir(testing_directory)
+    assert os.path.isdir(os.path.join(testing_directory, 'train'))
+    assert os.path.isdir(os.path.join(testing_directory, 'val'))
+    with open('testing_directory.txt', 'w') as f:
+        f.write(testing_directory)
+    return testing_directory
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Setting up image directory for opencv transforms testing')
+    parser.add_argument('-d', '--datadir', default=os.getcwd(),
+                        help='Imagenet directory')
+
+    args = parser.parse_args()
+
+    setup_testing_directory(args.datadir)
---
+++ tests/test_spatial.py
@@ -0,0 +1,54 @@
+import glob
+import numpy as np
+import random
+from typing import Union
+
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+
+from torchvision import transforms as pil_transforms
+from torchvision.transforms import functional as F_pil
+from opencv_transforms import transforms
+from opencv_transforms import functional as F
+
+from setup_testing_directory import get_testing_directory
+
+TOL = 1e-4
+
+datadir = get_testing_directory()
+train_images = glob.glob(datadir + '/**/*.JPEG', recursive=True)
+train_images.sort()
+print('Number of training images: {:,}'.format(len(train_images)))
+
+random.seed(1)
+imfile = random.choice(train_images)
+pil_image = Image.open(imfile)
+image = cv2.cvtColor(cv2.imread(imfile, 1), cv2.COLOR_BGR2RGB)
+
+
+def L1(pil: Union[Image, np.ndarray], np_image: np.ndarray) -> float:
+    return np.abs(np.asarray(pil) - np_image).mean()
+
+
+def test_resize():
+    pil_resized = pil_transforms.Resize((224, 224))(pil_image)
+    resized = transforms.Resize(224)(image)
+    l1 = L1(pil_resized, resized)
+    assert l1 - 88.9559 < TOL
+
+def test_rotation():
+    random.seed(1)
+    pil = pil_transforms.RandomRotation(10)(pil_image)
+    random.seed(1)
+    np_img = transforms.RandomRotation(10)(image)
+    l1 = L1(pil, np_img)
+    assert l1 - 86.7955 < TOL
+
+def test_five_crop():
+    pil = pil_transforms.FiveCrop((224, 224))(pil_image)
+    cv = transforms.FiveCrop((224, 224))(image)
+    pil_stacked = np.hstack([np.asarray(i) for i in pil])
+    cv_stacked = np.hstack(cv)
+    l1 = L1(pil_stacked, cv_stacked)
+    assert l1 - 22.0444 < TOL
